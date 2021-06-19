ab756f567d833067811a9ba529aa88484e660b50
Drk <2442854604@qq.com>
2021-06-19 19:52:32 +0800

    第一次提交信息

---
+++ src/views/home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="home" class="home-nav">
+    <nav-bar><div slot="center">购物街</div></nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "@/components/common/navbar/NavBar";
+  import {getHomeMutidata} from "@/network/home";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    },
+    created() {
+      //1.请求多个数据
+      getHomeMutidata().then(
+        res => {
+        console.log(res);
+      })
+    }
+  }
+</script>
+
+<style scoped>
+.home-nav{
+  background-color: var(--color-tint);
+  color: #f6f6f6;
+}
+</style>
